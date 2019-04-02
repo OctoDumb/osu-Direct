3d5e6497803a934fc0cccbb7c78e63d5c5e076e1
Vlad <vladik0701@gmail.com>
2019-04-02 19:04:02 +0300

    Cleaning shit

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const electron = require('electron')
+const electron = require('electron');
 const { app, BrowserWindow } = electron;
-app.setAppUserModelId("octoDumb.osuDirect.Desktop.alpha");
+app.setAppUserModelId("octoDumb.osuDirect.Desktop"); // Dunno why but ok i'll leave it here
 
 let mainWindow;
 
 const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
 	if(mainWindow) {
-		if (mainWindow.isMinimized()) mainWindow.restore();
 		mainWindow.focus();
-        if(argv[1]) mainWindow.webContents.executeJavaScript(`donwloadBeatmapset(${argv[1].split("osu://dl/")[1].split("/")[0]}, 'Redirect', 'Redirected from website')`);
         return true;
 	} else {
-        createWindow();
         return false;
-    };
-})
+    }
+});
 
 
 if (shouldQuit) {
@@ -29,18 +26,16 @@ function createWindow() {
   	mainWindow.loadFile('login.html');
 
     mainWindow.on('close', function (event) {
-        win = null
-    })
+        win = null;
+    });
 
     mainWindow.on('minimize', function (event) {
         mainWindow.minimize();
-    })
+    });
 }
 
 app.on('window-all-closed', () => {
-  app.quit()
-})
+  app.quit();
+});
 
 app.on('ready', createWindow);
-
-app.setAsDefaultProtocolClient("osu");
