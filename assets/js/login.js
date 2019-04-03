6c08c5b01bf126e382d7bdc80c04f5155453fd31
Vlad <vladik0701@gmail.com>
2019-04-03 14:31:05 +0300

    Login screen and stuff

---
+++ assets/js/login.js
@@ -0,0 +1,39 @@
+var { remote, ipcRenderer: ipc } = require('electron');
+
+var $ = require('jquery');
+
+$(document).ready(function() {
+    ipc.send('mainWindowLoaded', true);
+
+    $("#login-form").submit(e => {
+        e.preventDefault();
+    });
+
+    $(".minimize").click(function() {
+        remote.getCurrentWindow().minimize();
+    });
+
+    $(".close").click(function() {
+        remote.getCurrentWindow().close();
+    });
+
+    $("#login-btn").click(function(e) {
+        e.preventDefault();
+        $(".status").html('<div class="spinner-border" style="width:40px;height:40px;" role="status"></div>');
+        let logged = ipc.sendSync('login', {u: $("#usernameInput").val(), p: $("#passwordInput").val()});
+        if(logged) {
+            $(".status").html('<div class="alert alert-success" role="alert">Successfully logged in!</div>');
+        } else {
+            $(".status").html('<div class="alert alert-danger" role="alert">You have provided incorrect data! Check yourself and try again!</div>');
+        }
+    });
+});
+
+function passwordVisible() {
+    if($("#passwordInput").attr("type") == "password") {
+        $("#passwordInput").attr("type", "text");
+    } else {
+        $("#passwordInput").attr("type", "password");
+    }
+    $("#passVis").toggleClass("fa-eye").toggleClass("fa-eye-slash");
+}
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain: ipc } = electron;
+const userData = app.getPath('userData').split('\\').join('/');
+const fs = require('fs');
+if(!fs.existsSync(`${userData}/cookies.data`)) fs.writeFileSync(`${userData}/cookies.data`, "[]");
+var CookieStore = require('tough-cookie-filestore');
+const request = require('request').defaults({
+    jar: new CookieStore(`${userData}/cookies.data`)
+});
+
 app.setAppUserModelId("octoDumb.osuDirect.Desktop"); // Dunno why but ok i'll leave it here
 
 let mainWindow, loadingWindow;
@@ -22,6 +30,11 @@ function createWindow() {
     loadingWindow.loadFile('loading.html');
     loadingWindow.on('ready-to-show', () => {
         loadingWindow.show();
+        mainWindow = new BrowserWindow({width: 800, height: 600, frame: false, icon: __dirname + '/icon.ico', show: false});
+        mainWindow.loadFile(fs.existsSync(`${userData}/login.data`) ? 'index.html' : 'login.html');
+        mainWindow.on('ready-to-show', () => {
+            mainWindow.show();
+        });
     });
 }
 
@@ -30,3 +43,27 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', createWindow);
+
+/* IPC Section */
+
+ipc.on('mainWindowLoaded', (event) => {
+    if(loadingWindow) {
+        loadingWindow.close();
+        loadingWindow = null;
+    }
+});
+
+ipc.on('login', (event, args) => {
+    request.post({
+        url: 'https://osu.ppy.sh/forum/ucp.php?mode=login',
+        formData: {
+            login: "Login",
+            username: args.u,
+            password: args.p
+        }
+    }, function(err, res, body) {
+        fs.writeFileSync("./test.html", body);
+        var incorrect = /You have specified an incorrect/i;
+        event.returnValue = !incorrect.test(body);
+    });
+});
